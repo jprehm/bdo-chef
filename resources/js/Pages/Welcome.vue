8e3abfdd0761c782116cf371535f0207d2b47d7b
Jeff Prehm <japrehm@gmail.com>
2024-04-28 09:15:06 -0500

    remove team and recruitment section

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -23,7 +23,6 @@ const props = defineProps({
     }
 });
 
-
 const navigation = [
     { name: 'Recipes', href: route('recipes') },
     { name: 'Ingredients', href: route('ingredients.index') },
@@ -32,16 +31,6 @@ const stats = [
     { label: 'Recipes Supported', value: props.recipe_count },
     { label: 'Ingredients Supported', value: props.ingredient_count },
     { label: 'Chefs Registered', value: props.user_count },
-]
-
-const team = [
-    {
-        name: 'Jeff Prehm',
-        role: 'Founder',
-        imageUrl:
-            '/images/jeff.webp',
-        location: 'St. Louis, MO, USA',
-    },
 ]
 const mobileMenuOpen = ref(false)
 </script>
@@ -163,62 +152,6 @@ const mobileMenuOpen = ref(false)
                                 <dd class="text-3xl font-semibold tracking-tight text-white">{{ stat.value }}</dd>
                             </div>
                         </dl>
-                    </div>
-                </div>
-
-                <!-- Image section -->
-                <!-- <div class="mt-32 sm:mt-40 xl:mx-auto xl:max-w-7xl xl:px-8">
-                <img src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2894&q=80"
-                    alt="" class="aspect-[9/4] w-full object-cover xl:rounded-3xl" />
-            </div> -->
-
-                <!-- Team section -->
-                <div class="mx-auto mt-8 max-w-7xl px-6 sm:mt-8 lg:px-8">
-                    <div class="mx-auto max-w-2xl lg:mx-0">
-                        <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Our team</h2>
-                        <p class="mt-6 text-lg leading-8 text-gray-300">This is a passion project, made by a small team.
-                        </p>
-                    </div>
-                    <ul role="list"
-                        class="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-14 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 xl:grid-cols-4">
-                        <li v-for="person in team" :key="person.name">
-                            <img class="aspect-[14/13] w-full rounded-2xl object-cover" :src="person.imageUrl" alt="" />
-                            <h3 class="mt-6 text-lg font-semibold leading-8 tracking-tight text-white">{{ person.name }}
-                            </h3>
-                            <p class="text-base leading-7 text-gray-300">{{ person.role }}</p>
-                            <p class="text-sm leading-6 text-gray-500">{{ person.location }}</p>
-                        </li>
-                    </ul>
-                </div>
-
-                <!-- CTA section -->
-                <div class="relative isolate -z-10 mt-16 sm:mt-40">
-                    <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                        <div
-                            class="mx-auto flex max-w-2xl flex-col gap-16 bg-white/5 px-6 py-16 ring-1 ring-white/10 sm:rounded-3xl sm:p-8 lg:mx-0 lg:max-w-none lg:flex-row lg:items-center lg:py-20 xl:gap-x-20 xl:px-20">
-                            <img class="h-96 w-full flex-none rounded-2xl object-cover shadow-xl lg:aspect-square lg:h-auto lg:max-w-sm"
-                                src="https://images.unsplash.com/photo-1519338381761-c7523edc1f46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-                                alt="" />
-                            <div class="w-full flex-auto">
-                                <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Join the team</h2>
-                                <p class="mt-3 text-lg leading-8 text-gray-300">Do you know Laravel and vuejs, and want
-                                    to
-                                    contribute?</p>
-                                <p class="mt-3 text-lg leading-8 text-gray-300">Are you a UX/UI and want to provide
-                                    design
-                                    help?</p>
-                                <div class="mt-5 flex">
-                                    <a href="#" class="text-sm font-semibold leading-6 text-indigo-400">Contact me
-                                        <span aria-hidden="true">&rarr;</span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="absolute inset-x-0 -top-16 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl"
-                        aria-hidden="true">
-                        <div class="aspect-[1318/752] w-[82.375rem] flex-none bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-25"
-                            style="clip-path: polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)" />
                     </div>
                 </div>
             </main>
